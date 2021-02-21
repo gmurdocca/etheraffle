b0416b3724fd16867a8ddea5a8010d030e652ade
George Murdocca <george@linuxdojo.com>
2021-02-21 16:22:24 +1100

    fix mnemonic generation instructions

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Raffle DApp on Ethereum
 
 ## Setup
 
-1. Create an account at infura.io
-2. Create an Ethereum project at infura.io, set endpoint to Ropsten, note the PROJECT ID
-3. Create a mnemonic: `make mnemonic`
-4. Copy `.secrets.json.example` to `.secrets.json`, edit, and project_id and mnemonic values from above steps
-5. Run `make test`
+- Create an account at infura.io
+- Create an Ethereum project at infura.io, set endpoint to Ropsten, note the PROJECT ID
+- Install MetaMask browser plugin, select Ropsten network, create a wallet, grab mnemonic value from Settings -> Security and Privacy -> Reveal Seed Phrase
+- Copy `.secrets.json.example` to `.secrets.json`, set `project_id` and `mnemonic` values from above steps
+- Run `make test`
--- truffle-config.js
+++ truffle-config.js
@@ -75,11 +75,12 @@ module.exports = {
 
     ropsten: {
       provider: () => new HDWalletProvider(config.mnemonic, `https://ropsten.infura.io/v3/${config.project_id}`),
-      network_id: 3,       // Ropsten's id
-      gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      network_id: 3,          // Ropsten's id
+      gas: 5500000,           // Ropsten has a lower block limit than mainnet
+      gasPrice: 10000000000,  // 10 gwei (in wei) (default: 100 gwei)
+      confirmations: 2,       // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,     // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: false       // Skip dry run before migrations? (default: false for public nets )
     },
   },
 
