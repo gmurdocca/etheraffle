6bdd5b48cd24a0195c8696dce0e4daa1a4f0377e
George Murdocca <george@linuxdojo.com>
2021-02-27 23:42:08 +1100

    add some code comments

--- contracts/Etheraffle.sol
+++ contracts/Etheraffle.sol
@@ -9,7 +9,7 @@ contract Etheraffle is Ownable {
     event Deposit(address user, uint amount);
     event Withdrawal(address user, uint amount);
 
-    mapping (address => uint256) private deposits;
+    mapping (address => uint256) private deposits; // store deposit balance for each sender
     uint min_gwei_deposit = 0;              // min amount depositable by competitors in this raffle
     uint max_gwei_deposit = 0;              // max amount depositable by competitors in this raffle
     uint draw_date;                         // draw function is only possible after a block is mined on or after this unix epoch date
@@ -41,7 +41,7 @@ contract Etheraffle is Ownable {
     }
 
     function withdraw(uint withdraw_amount) external {
-        // only allow withdrawal if the raffle is not ended.
+        // only allow withdrawal if the raffle has not ended.
         require(ended == false, "Sorry, this raffle has ended.");
         require(block.timestamp < draw_date, "Sorry, this raffle's draw date has passed and funds are pending delivery to winners.");
         require(deposits[msg.sender] >= withdraw_amount, "Insufficient balance for withdrawal request.");
@@ -55,21 +55,21 @@ contract Etheraffle is Ownable {
     }
 
     function set_min_gwei_deposit(uint _min_gwei_deposit) public onlyOwner {
-        // allow the contract owner to adjust the min deposit value
+        // conditionally allow the contract owner to adjust the min deposit value
         require(is_mutable == true, "This raffle's settings can not be changed");
         require(_min_gwei_deposit < max_gwei_deposit, "min deposit must be less than max deposit");
         min_gwei_deposit = _min_gwei_deposit * 1000000000; // convert gwei to wei
     }
 
     function set_max_gwei_deposit(uint _max_gwei_deposit) public onlyOwner {
-        // allow the contract owner to adjust the max deposit value
+        // conditionally allow the contract owner to adjust the max deposit value
         require(is_mutable == true, "This raffle's settings can not be changed");
         require(_max_gwei_deposit > min_gwei_deposit, "max deposit must be greater than min deposit");
         max_gwei_deposit = _max_gwei_deposit * 1000000000; // convert gwei to wei
     }
 
     function set_draw_date(uint _draw_date) public onlyOwner {
-        // allow the contract owner to adjust the draw date 
+        // conditionally allow the contract owner to adjust the draw date
         require(is_mutable == true, "This raffle's settings can not be changed");
         require(_draw_date > block.timestamp, "Draw date must be in the future" );
         draw_date = _draw_date;
